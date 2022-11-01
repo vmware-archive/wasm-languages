10495082d81bb8b3d81d0e0f4a6a2f61b0badb95
Zalim Bashorov <zalim.bashorov@jetbrains.com>
2022-11-01 21:18:12 +0100

    Use the same version of kotlin in nodejs-hello-world as in browser-hello-world and update node for compatibility

--- kotlin/nodejs-hello-world/build.gradle.kts
+++ kotlin/nodejs-hello-world/build.gradle.kts
@@ -2,14 +2,17 @@ import org.jetbrains.kotlin.gradle.targets.js.nodejs.NodeJsRootExtension
 import org.jetbrains.kotlin.gradle.targets.js.nodejs.NodeJsRootPlugin
 
 plugins {
-    kotlin("multiplatform") version "1.7.20"
+//    kotlin("multiplatform") version "1.7.20" // Compatible with Node 18.12.0 (current LTS)
+    kotlin("multiplatform") version "1.8.0-Beta-198" // Compatible with Node 19.0.0
 }
 
 group = "com.example"
 version = "1.0-SNAPSHOT"
 
 repositories {
+    mavenLocal()
     mavenCentral()
+    maven("https://maven.pkg.jetbrains.space/kotlin/p/kotlin/dev")
 }
 
 kotlin {
@@ -24,5 +27,6 @@ kotlin {
 }
 
 rootProject.plugins.withType(NodeJsRootPlugin::class.java) {
-    rootProject.the<NodeJsRootExtension>().nodeVersion = "18.12.0"
+//    rootProject.the<NodeJsRootExtension>().nodeVersion = "18.12.0"
+    rootProject.the<NodeJsRootExtension>().nodeVersion = "19.0.0"
 }
