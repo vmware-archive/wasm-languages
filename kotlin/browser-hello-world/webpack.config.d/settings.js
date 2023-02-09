8b13149d8babfc1cd1cd88c77f0e54823bb09279
Sébastien Deleuze <sdeleuze@vmware.com>
2023-02-09 08:46:22 +0100

    Update Kotlin browser sample to 1.8.20-Beta

--- kotlin/browser-hello-world/webpack.config.d/settings.js
+++
@@ -1,8 +0,0 @@
-// noinspection JSUnresolvedVariable
-
-config.devServer?.static?.push(
-    __dirname + "/kotlin/"
-)
-
-// Workaround for KT-53790
-config.externals = ["node:module", "."]; // TODO remove after Kotlin 1.8.20
